178a90ff4796582d5fb75f383aaaa816525c4b73
khoa16122004 <khoatr.work@gmail.com>
2025-10-30 15:29:41 +0700

    un-cmt es 1lambda

--- modules/attack/attack.py
+++ modules/attack/attack.py
@@ -80,7 +80,7 @@ class ES_1_Lambda(BaseAttack):
                 sigma *= self.c_dec            
                 # sigma = max(1e-6, sigma)     
             
-            # print("Best loss: ", f_m, " l2: ", l2_m)
+            print("Best loss: ", f_m, " l2: ", l2_m)
 
             history.append([float(f_m), float(l2_m)])
             if self.is_success(f_m):
